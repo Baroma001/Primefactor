3a5d04886abd332d243ae2c006893cd30a2fc357
Baroma001 <49646668+Baroma001@users.noreply.github.com>
2024-12-23 21:25:03 +0100

    Initial commit

---
+++ largeNumber.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yannick Hettinga on 22.12.24.
+//
+
+#include "largeNumber.h"
---
+++ largeNumber.h
@@ -0,0 +1,17 @@
+//
+// Created by Yannick Hettinga on 22.12.24.
+//
+
+#ifndef LARGENUMBER_H
+#define LARGENUMBER_H
+#include <string>
+#include <boost/multiprecision/cpp_int.hpp>
+
+
+
+class largeNumber {
+};
+
+
+
+#endif //LARGENUMBER_H
